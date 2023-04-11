283af15b6492198e38900b356be8bd77e42f65fb
Neto test <zilya.zakirova1991@gmail.com>
2023-04-11 14:42:12 +0300

    new

--- src/main/java/ru/netology/statistic/Main.java
+++ src/main/java/ru/netology/statistic/Main.java
@@ -1,4 +1,4 @@
-package ru.netology.services;
+package ru.netology.statistic;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/test/java/StatisticsServiceTest.java
@@ -0,0 +1,21 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Assertions;
+import ru.netology.statistic.StatisticsService;
+
+public class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
